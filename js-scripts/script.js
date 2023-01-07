89b7d7b574508aeb572924f1e0b50293cf69ac1e
Cristian Weber <47835197+ceiferking@users.noreply.github.com>
2023-01-06 23:28:01 -0300

    Update script.js

---
+++ js-scripts/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
+const validainput = ({target}) => {
+    if(target.value.length > 2) {
+        button.removeAttribute('disabled');
+    } else{
+        button.setAttribute('disabled');
+    }
+}
+
+const subGo = (event) => {
+    event.preventDefault();
+  
+    localStorage.setItem('Player', input.value);
+    window.location = '/pages/game.html';
+
+}
+
+input.addEventListener('input', validainput);
+form.addEventListener('submit', subGo);
